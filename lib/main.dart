7ecca9583f37c9b92d35e8bdf252b2c7611f6b22
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2022-04-16 18:34:35 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PortfolioApp());
+}
+
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio',
+      home: Container(),
+    );
+  }
+}
