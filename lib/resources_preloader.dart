671d8aff200a4a3269e6a4c8620c2eb5866629cf
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2022-05-20 10:09:25 +0300

    Add image preloader

---
+++ lib/resources_preloader.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/painting.dart';
+
+import 'app_resources.dart';
+import 'utils/preload_image.dart';
+
+Future<void> preloadResources() async {
+  await Future.wait([
+    preloadImage(const AssetImage(AppResources.home)),
+    preloadImage(const AssetImage(AppResources.about)),
+  ]);
+}
